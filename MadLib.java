02481596c8a4538d2d560daaa8f0c4ee6f1de105
Sprouse <connors38536@stu.powayusd.com>
2022-09-21 14:40:55 -0700

    first print line

--- MadLib.java
+++ MadLib.java
@@ -1,7 +1,17 @@
+/*
+1.2.10 Mad Libs
+
+Authors: Connor Sprouse, Palak Jain
+Date: 9/21/22
+Course: APCSA Period 4 Mrs. Woldseth
+
+Description: 
+    Madlibs game that takes user input for parts of speech to create a funny story.
+*/
 public class MadLib
 {
     public static void main (String[] args)
     {
-        //code
+        System.out.println("Welcome to our Madlibs game!");
     }
 }
