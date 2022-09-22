017d61a7eab777b1d77c6b53478dc7392d253b76
Sprouse <connors38536@stu.powayusd.com>
2022-09-22 14:27:55 -0700

    finished

--- MadLib.java
+++ MadLib.java
@@ -8,10 +8,23 @@ Course: APCSA Period 4 Mrs. Woldseth
 Description: 
     Madlibs game that takes user input for parts of speech to create a funny story.
 */
+import java.util.Scanner;
 public class MadLib
 {
     public static void main (String[] args)
     {
         System.out.println("Welcome to our Madlibs game!");
+        String madLib = "The boy went to the <noun>, <verb> with his friends, then walked home and ate <noun>.";
+        //String madLib = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
+        Scanner myObj = new Scanner(System.in);
+
+        while (madLib.indexOf("<")!= -1)
+        {
+            System.out.println("Enter a " + madLib.substring(madLib.indexOf("<") + 1, madLib.indexOf(">")));
+            String userInput = myObj.nextLine();
+            String temp = madLib.substring(madLib.indexOf("<"), madLib.indexOf(">") + 1);
+            madLib = madLib.replaceFirst(temp, userInput);
+        }        
+            System.out.println(madLib); 
     }
 }
